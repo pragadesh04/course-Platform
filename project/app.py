b6810c966e1f9ccd65c9322470ef951b18472c48
Praga866 <pragaofficial004@gmail.com>
2024-08-05 00:17:06 +0530

    added login and signup page

--- project/app.py
+++ project/app.py
@@ -33,5 +33,13 @@ def home():
 def course():
     return render_template("courses.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)

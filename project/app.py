0732c8c1e1f1f6f938ad32f62a31a23ce6a69dfc
Praga866 <pragaofficial004@gmail.com>
2024-08-05 23:24:08 +0530

    UI changes

--- project/app.py
+++ project/app.py
@@ -1,29 +1,29 @@
-from flask import Flask, request, render_template
-# from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-# from flask_wtf import wtforms
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import input_required, length, ValidationError
+from wtforms.validators import InputRequired, Length, ValidationError
 
 app = Flask(__name__)
 
+# Configuring the SQLAlchemy Database URI
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datas.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class data(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False, unique = True)
-    password = db.Column(db.String(20), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
 
+    def __repr__(self) -> str:
+        return f"datas('{self.username}', '{self.password}')"
+
+# RegisterForm with correct validators
 # class registerForm(FlaskForm):
-#     username = StringField("Username", validators=[input_required(), length(min=4, max=20)], render_kw={"placeholder":"Username"})
-#     password = StringField("Username", validators=[input_required(), length(min=4, max=20)], render_kw={"placeholder":"Password"})
-#     submit = SubmitField("Registered")
-
-def __repr__(self) -> str:
-    return f"datas('{self.username}', '{self.password}')"
+#     username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+#     password = PasswordField("Password", validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+#     submit = SubmitField("Register")
 
 @app.route("/")
 @app.route("/home")
@@ -38,8 +38,16 @@ def course():
 def login():
     return render_template("login.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirmPass = request.form.get('confirmPass')
+        if(password == confirmPass):
+            print("name: {}\npass: {}".format(username, password))
+        else:
+            print("Pasword does not match!!")
     return render_template("signup.html")
 
 @app.route("/about")
